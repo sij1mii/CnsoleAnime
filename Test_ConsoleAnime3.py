ea1553b925f80cc9c6a7e1cea239fab11a6084bf
sij1mii <kuniyoshi9@gmail.com>
2020-02-24 19:27:39 +0900

    add new file

--- Test_ConsoleAnime3.py
+++ Test_ConsoleAnime3.py
@@ -5,30 +5,26 @@ import time
 def Loop1():
   while True:
     n = 0
-    x = 10
+    x = 11
     count = 0
 
     board = [['□ ' for n in range(21)]for i in range(21)]
     #nが10以上になるとエラー
-    #nが10になったら0を代入
-    while count <= 17:
-      count += 1
+    #nが10になったら1を代入
+    while count < 24:
       n += 1
       x -= 1
-      if n > 9:
-        n = 0
-      if x < 1:
-        x = 10
+      count += 1
 
       #下から表示
-      if 0 < count < 5:
-        board[(x+10)-1][9+n]='● '#右
-        board[(x+10)-1][x+1]='● '#左
+      if count < 5:
+        board[(x+10)-2][9+n]='● '#右
+        board[(x+10)-2][x]='● '#左
       elif 4 < count < 7:
-        board[(x+10)-1][13]='● '
-        board[(x+10)-1][7]='● '
+        board[(x+10)-2][13]='● '
+        board[(x+10)-2][7]='● '
       elif 6 < count < 9:
-        board[12][x+9]='● '
+        board[12][x+8]='● '
         board[12][n+1]='● '
       elif 8 < count < 10:
         board[13][10]='● '
@@ -36,18 +32,32 @@ def Loop1():
       #消す
       if 9 < count:
         board[18][10]='  ' 
-      if 9 < count < 14:
-        board[(x+10)-2][10+n]='  '
+      if 10 < count < 14:
+        board[(x+10)-2][9+n]='  '
         board[(x+10)-2][x]='  '
-      elif 13 < count < 16:
+      elif 13 < count < 17:
         board[(x+10)-2][13]='  '
         board[(x+10)-2][7]='  '
-      elif 15 < count < 18:
+      elif 16 < count < 19:
         board[12][x+8]='  '
-        board[12][n+2]='  '
-      elif 17 < count < 20:
+        board[12][n+1]='  '
+      elif 18 < count < 20:
         board[13][10]='  '
-      
+
+      #*下から
+      if 13 < count < 18:
+        board[x+12][6+n]='* '
+        board[x+12][x+3]='* '
+      elif 17 < count < 21:
+        board[x+12][14]='* '
+        board[x+12][6]='* '
+      elif 20 < count < 23:
+        board[x+2][x+3]='* '
+        board[x+2][n+6]='* '
+      elif 22 < count < 25:
+        board[n+8][x+3]='* '
+        board[n+8][n+6]='* '
+
       #リストの括弧など非表示 ボード表示
       for board2 in board:
         print(' '.join(board2))
@@ -56,6 +66,14 @@ def Loop1():
       print('x=',x)
       print(count)
       time.sleep(0.3)
-      os.system('clear')
+      #os.system('clear')
+
+      #count += 1
+      #n += 1
+      #x -= 1
+      if n == 10:
+        n = 0
+      if x == 1:
+        x = 11
 	
 Loop1()
