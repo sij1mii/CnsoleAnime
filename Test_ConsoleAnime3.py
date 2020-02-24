a9efec4d2a5cfe30418cc2fd91853e12ddef72e0
sij1mii <kuniyoshi9@gmail.com>
2020-02-24 20:54:13 +0900

    add new file

--- Test_ConsoleAnime3.py
+++ Test_ConsoleAnime3.py
@@ -2,6 +2,11 @@
 import os
 import time
 
+A = '● '
+B = '□ '
+C = '* '
+D = '  '
+
 def Loop1():
   while True:
     n = 0
@@ -18,45 +23,45 @@ def Loop1():
 
       #下から表示
       if count < 5:
-        board[(x+10)-2][9+n]='● '#右
-        board[(x+10)-2][x]='● '#左
+        board[(x+10)-2][9+n]=A#右
+        board[(x+10)-2][x]=A#左
       elif 4 < count < 7:
-        board[(x+10)-2][13]='● '
-        board[(x+10)-2][7]='● '
+        board[(x+10)-2][13]=A
+        board[(x+10)-2][7]=A
       elif 6 < count < 9:
-        board[12][x+8]='● '
-        board[12][n+1]='● '
+        board[12][x+8]=A
+        board[12][n+1]=A
       elif 8 < count < 10:
-        board[13][10]='● '
+        board[13][10]=A
 
       #消す
       if 9 < count:
-        board[18][10]='  ' 
+        board[18][10]=B 
       if 10 < count < 14:
-        board[(x+10)-2][9+n]='  '
-        board[(x+10)-2][x]='  '
-      elif 13 < count < 17:
-        board[(x+10)-2][13]='  '
-        board[(x+10)-2][7]='  '
-      elif 16 < count < 19:
-        board[12][x+8]='  '
-        board[12][n+1]='  '
-      elif 18 < count < 20:
-        board[13][10]='  '
+        board[(x+10)-3][10+n]=B
+        board[(x+10)-3][x-1]=B
+      elif 13 < count < 16:
+        board[x+7][13]=B
+        board[x+7][7]=B
+      elif 15 < count < 18:
+        board[12][x+7]=B
+        board[12][n+2]=B
+      elif 17 < count < 19:
+        board[13][10]=B
 
       #*下から
       if 13 < count < 18:
-        board[x+12][6+n]='* '
-        board[x+12][x+3]='* '
+        board[x+12][6+n]=C
+        board[x+12][x+3]=C
       elif 17 < count < 21:
-        board[x+12][14]='* '
-        board[x+12][6]='* '
+        board[x+12][14]=C
+        board[x+12][6]=C
       elif 20 < count < 23:
-        board[x+2][x+3]='* '
-        board[x+2][n+6]='* '
+        board[x+2][x+3]=C
+        board[x+2][n+6]=C
       elif 22 < count < 25:
-        board[n+8][x+3]='* '
-        board[n+8][n+6]='* '
+        board[n+8][x+3]=C
+        board[n+8][n+6]=C
 
       #リストの括弧など非表示 ボード表示
       for board2 in board:
